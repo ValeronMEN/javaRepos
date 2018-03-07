e8d0483fba68b59d3f20dbffcc12c324d66f64f1
Valeriy <valeroon.men@gmail.com>
2018-03-08 01:57:46 +0200

    CG Lab 2 comments updated

--- Computer Graphics/lab2/cg_lab2/src/cg_lab2/Dwelling.java
+++ Computer Graphics/lab2/cg_lab2/src/cg_lab2/Dwelling.java
@@ -14,6 +14,7 @@ public class Dwelling extends JPanel implements ActionListener {
 	private static int maxWidth;
 	private static int maxHeight;
 	
+	// the roof coords
 	double points[][] = {
 			{ -96, -25 }, { 96, -25 }, { 0, -80 },
 			};
@@ -38,17 +39,17 @@ public class Dwelling extends JPanel implements ActionListener {
 	
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		
 		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
 				rh.put(RenderingHints.KEY_RENDERING,
 				RenderingHints.VALUE_RENDER_QUALITY);
 				g2d.setRenderingHints(rh);
 		
+		// the background color setting
 		g2d.setBackground(new Color(25,25,112));
 		g2d.clearRect(0, 0, maxWidth, maxHeight);
 		
-		// set the origin to the centre
+		// set the origin to the center
 		g2d.translate(maxWidth/2, maxHeight/2);
 		
 		BasicStroke bs2 = new BasicStroke(16, BasicStroke.CAP_ROUND, 
@@ -57,38 +58,47 @@ public class Dwelling extends JPanel implements ActionListener {
 				g2d.drawRect(-(radius + radiusExtention), -(radius + radiusExtention),
 						(radius + radiusExtention)*2, (radius + radiusExtention)*2);
 		
-		// movement transformation
+		// the movement transformation
 		g2d.translate(tx, ty);
 		
+		// the roof triangle setting
 		GeneralPath roof = new GeneralPath();
 		roof.moveTo(points[0][0], points[0][1]);
 		for (int k = 1; k < points.length; k++)
 			roof.lineTo(points[k][0], points[k][1]);
 		roof.closePath();
 		
-		// scale transformation
+		// the scale transformation
 		g2d.scale(scale, 0.99);
 		
+		// the roof drawing
 		g2d.setColor(Color.gray);
 		g2d.fill(roof);
-		g2d.setColor(Color.green);
+		
+		// the dwelling body drawing
 		g2d.setColor(new Color(139, 69, 19));
 		g2d.fillRect(-90, -25, 180, 115);
+		
+		// the windows drawing
 		g2d.setColor(Color.yellow);
 		g2d.fillRect(-70, 0, 30, 30);
 		g2d.fillRect(40, 0, 30, 30);
+		
+		// the stars drawing
 		g2d.setColor(Color.white);
 		g2d.fillRect(-70, -60, 10, 10);
 		g2d.fillRect(-40, -90, 10, 10);
 		g2d.fillRect(60, -60, 10, 10);
 		g2d.fillRect(30, -90, 10, 10);
 		
+		// the door drawing
 		GradientPaint gp = new GradientPaint(5, 25, 
 			    new Color(119,136,153), 20, 2, Color.black, true);
 	    g2d.setPaint(gp);
 	    g2d.fillRect(-20, -10, 40, 100);
 	    
-	    g2d.setColor(Color.white);
+	    // the grass drawing
+	    g2d.setColor(new Color(0, 100, 0));
 	    BasicStroke bs = new BasicStroke(8, BasicStroke.CAP_ROUND, 
 	    		BasicStroke.JOIN_BEVEL);
 	    g2d.setStroke(bs);
@@ -96,7 +106,7 @@ public class Dwelling extends JPanel implements ActionListener {
 	}
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Привіт, Java 2D!");
+		JFrame frame = new JFrame("Java 2D - one love!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(1000, 1000);
 		frame.setLocationRelativeTo(null);
@@ -112,12 +122,15 @@ public class Dwelling extends JPanel implements ActionListener {
 	}
 	
 	public void actionPerformed(ActionEvent e) {
+		// scaling
 		if ( scale < 0.01 ) {
 			deltaScale = -deltaScale;
 		} else if (scale > 0.99) {
 			deltaScale = -deltaScale;
 		}
+		scale += deltaScale;
 		
+		// movement
 		double radiusInSquare = Math.pow(radius, 2);
 		if (tx <= 0 && ty < 0){
 			tx -= deltaX;
@@ -133,7 +146,6 @@ public class Dwelling extends JPanel implements ActionListener {
 			ty = Math.abs(Math.sqrt(radiusInSquare - Math.pow(tx, 2)));
 		}
 		
-		scale += deltaScale;
 		repaint();
 	}
 }
